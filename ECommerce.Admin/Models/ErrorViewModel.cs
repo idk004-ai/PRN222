b627b09f2f25d9c67eed63e2b606e382b65ee999
idk004-ai <KhoiLNMHE180029@fpt.edu.vn>
2025-06-18 11:17:53 +0700

    Initial commit

---
+++ ECommerce.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ECommerce.Admin.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
